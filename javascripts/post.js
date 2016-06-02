34bef453f4f94a9d5d9ac83631be8590c05e3029
mapan1984 <mapan1984@outlook.com>
2016-06-02 12:46:48 +0800

    chage struct of blog

--- javascripts/post.js
+++ javascripts/post.js
@@ -127,3 +127,22 @@ window.onload = function(){
         },30);
     }
 }
+$(document).ready(function() { // 生成侧栏标题目录
+  $('#toc').toc();
+});
+/**
+ *  recommended configuration variables: edit and uncomment the section below to insert dynamic values from your platform or cms.
+ *  learn why defining these variables is important: https://disqus.com/admin/universalcode/#configuration-variables
+ */
+/*
+  var disqus_config = function () {
+  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+  };
+ */
+(function() {  // DON'T EDIT BELOW THIS LINE
+  var d = document, s = d.createElement('script');
+  s.src = '//skkmp.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
