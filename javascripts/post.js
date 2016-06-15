8161c6d6b1547d5ec82cb5c3988aba4e84e6301c
mapan1984 <mapan1984@outlook.com>
2016-06-15 12:35:50 +0800

    add button to control disqus

--- javascripts/post.js
+++ javascripts/post.js
@@ -92,22 +92,6 @@ $(document).ready(function() {
   $('#toc').toc(); // 生成侧栏标题目录
   $('body').scrollspy({ target: '#toc' }); // 开启滚动监听
 });
-/** 
- *  recommended configuration variables: edit and uncomment the section below to insert dynamic values from your platform or cms.
- *  learn why defining these variables is important: https://disqus.com/admin/universalcode/#configuration-variables
- */
-/*
-  var disqus_config = function () {
-  this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-  this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
- */
-(function() {  // DON'T EDIT BELOW THIS LINE
-  var d = document, s = d.createElement('script');
-  s.src = '//skkmp.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
 /* 
  * 生成回到顶部
  * 调整侧栏位置
@@ -160,4 +144,11 @@ window.onload = function(){
       }
     },30);
   }
+}
+// button 点击展开评论
+function showDis(){
+  var s = document.createElement('script');
+  s.src = '//skkmp.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (document.head || document.body).appendChild(s);
 }
